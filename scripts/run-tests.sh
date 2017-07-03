6ad45b16b596be8f835c64bdd9b150292fef58cb
Krzysztof Budnik <budnix@gmail.com>
2017-07-03 15:16:05 +0200

    WIP: Updated tests. #22

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 git clone https://github.com/handsontable/handsontable.git
+cd handsontable/
+npm install
+cd ..
+
+node src/main.js build -i handsontable/ -o handsontable_dist/ -a -U
